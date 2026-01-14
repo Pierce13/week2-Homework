02f23f182eeeb52250a3163fb41e91426ee93c6f
Pierce13 <168318223+Pierce13@users.noreply.github.com>
2026-01-14 08:55:18 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+//TODO: add the appropriate include statements (iostream and string)
+#include <iostream>
+#include <string>
+
+//TODOL create the main function
+int main()
+{
+    //TODOL make three variables: itemName (type string), price (type double), and quantity (type int)
+    std::string itemName;
+    double price;
+    int quantity;
+
+    const double TAX_RATE = 0.07;
+
+    std::cout << "Enter item name: ";
+    std::cin >> itemName;
+
+    //TODO: print the following to the screen: "Enter price per item: "
+    //TODO: have the user enter the price
+    std::cout << "Enter price per item: ";
+    std::cin >> price;
+
+    //TODO: print the following to the screen: "Enter quantity: "
+    //TODO: have the user enter the quantity
+    std::cout << "Enter quantity: ";
+    std::cin >> quantity;
+
+    //TODO: Set values to the subtotal, tax, and total. The subtotal would be the total price before taxes. The tax would be the subtotal multiplied by the tax rate, and 
+    //      the total would be the total plus the tax
+    double subtotal = price * quantity;
+    double tax = subtotal * TAX_RATE;
+    double total = subtotal + tax;
+
+    // Output
+    std::cout << "\n----- RECEIPT -----\n";
+    std::cout << "Item: " << itemName << std::endl;
+    std::cout << "Price: $" << price << std::endl;
+    std::cout << "Quantity: " << quantity << std::endl;
+    std::cout << "Subtotal: $" << subtotal << std::endl;
+    std::cout << "Tax (7%): $" << tax << std::endl;
+    std::cout << "Total: $" << total << std::endl;
+    std::cout << "-------------------\n";
+
+    return 0;
+}
